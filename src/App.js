38a48998cd908691e5781998baad3bf9f23851f8
Cao Anh Nhất <anhnhata8cool@gmail.com>
2020-09-10 18:53:56 +0700

    form handle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [yourNum, setYourNum] = React.useState(null);
+  const [comNum, setComNum] = React.useState(null);
+  const [isRun, setIsRun] = React.useState(false);
+  console.log("App -> isRun", isRun);
+
+  function clickStart() {
+    setIsRun(true);
+  }
+
+  function clickStop() {
+    setComNum(null);
+    setYourNum(null);
+    setIsRun(false);
+  }
+
+  function onChangeNumber(e) {
+    const value = parseInt(e.target.value);
+    if (e.charCode === 13) {
+      setYourNum(value);
+    }
+  }
+
+  React.useEffect(() => {
+    if (isRun) {
+      const num = Math.round(Math.random() * 10);
+      setComNum(num);
+    }
+  }, [yourNum]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>Số bạn chọn: {yourNum}</div>
+        <div>Số máy chọn: {comNum}</div>
+        <div>
+          Result:
+          {isRun && yourNum && comNum && (yourNum === comNum ? "Đúng" : "Sai")}
+        </div>
+      </div>
+      <div>
+        {isRun ? <input onKeyPress={onChangeNumber} type="number" /> : ""}
+      </div>
+      <div>
+        {!isRun ? (
+          <button onClick={clickStart}>Start</button>
+        ) : (
+          <button onClick={clickStop}>Stop</button>
+        )}
+      </div>
     </div>
   );
 }
